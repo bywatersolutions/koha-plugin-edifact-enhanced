c26624bc2d7abbf2cfd0754d2ce02ecff9829de9
Kyle M Hall <kyle@bywatersolutions.com>
2021-09-09 09:57:59 -0400

    Blackstone - Rename plugin module
    Blackstone - Update plugin description
    
    Blackstone - Update CI files

--- Koha/Plugin/Com/ByWaterSolutions/EdifactBlackstone/Edifact/Order.pm
+++ Koha/Plugin/Com/ByWaterSolutions/EdifactBlackstone/Edifact/Order.pm
@@ -1,4 +1,4 @@
-package Koha::Plugin::Com::ByWaterSolutions::EdifactEnhanced::Edifact::Order;
+package Koha::Plugin::Com::ByWaterSolutions::EdifactBlackstone::Edifact::Order;
 
 use Modern::Perl;
 use utf8;
