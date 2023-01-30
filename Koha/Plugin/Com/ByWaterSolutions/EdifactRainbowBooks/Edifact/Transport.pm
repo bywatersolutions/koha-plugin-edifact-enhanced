0dcad379d636fc5b5e95245fede731b59100664f
Kyle M Hall <kyle@bywatersolutions.com>
2023-01-30 13:00:57 -0500

    Rainbow Books - Rename module to EdifactRainbowBooks

--- Koha/Plugin/Com/ByWaterSolutions/EdifactRainbowBooks/Edifact/Transport.pm
+++ Koha/Plugin/Com/ByWaterSolutions/EdifactRainbowBooks/Edifact/Transport.pm
@@ -1,4 +1,4 @@
-package Koha::Plugin::Com::ByWaterSolutions::EdifactEnhanced::Edifact::Transport;
+package Koha::Plugin::Com::ByWaterSolutions::EdifactRainbowBooks::Edifact::Transport;
 
 # Copyright 2014 PTFS-Europe Ltd
 #
