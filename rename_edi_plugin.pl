ad48da9280bbf9cae40b6a52ef55e2c0c0729806
Kyle M Hall <kyle@bywatersolutions.com>
2026-03-12 20:18:59 -0400

    Revert "GitHub Actions - Don't rename main.yml"
    This reverts commit 7eb3d9a05ae5ff26bfc20f264129d6ca9a8ad5f9.

--- rename_edi_plugin.pl
+++ rename_edi_plugin.pl
@@ -62,7 +62,6 @@ find(
             return unless -f $_;
             return if $File::Find::name =~ m{/\.git/};
             return if $File::Find::name =~ m{/node_modules/};
-            return if $File::Find::name =~ m{/release-sync.yml/};
             return if $File::Find::name =~ /rename_edi_plugin.pl/;
 
             my $content = read_file($_);
